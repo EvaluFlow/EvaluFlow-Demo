1ec6877cf6b8952e6e50d402d614997ace1f126f
illyum <90023277+itzilly@users.noreply.github.com>
2023-11-19 03:18:10 -0700

    Fix import path

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import Login from "./App"
+
+import App from "./Views/App"
+
 import "./styles.css"
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
