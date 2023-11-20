3de908bf2dffbf0b54481de1a9229d9e1ed664de
illyum <90023277+itzilly@users.noreply.github.com>
2023-11-19 22:18:42 -0700

    Add simple login page

--- app/src/Views/Login.jsx
+++ app/src/Views/Login.jsx
@@ -9,23 +9,25 @@ function Login() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
-    async function tester() {
-        
+    const [status, setStatus] = useState("Not logged in")
+
+    function tester(usrnm, pswd) {
+        if (usrnm === "admin" && pswd === "admin") {
+            setStatus("User logged in")
+        } else {
+            setStatus("Invalid Credentials")
+        }
     }
 
-    function onLoginClick(e) {
-        e.preventDefault()
-        console.log("Login button clicked!")
-    }
-1
     return (
         <div>
             <h1>Welcome to Tauri React!</h1>
             <form onSubmit={(e) => {e.preventDefault(); tester(username, password)}}>
-                <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-                <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
                 <button type="submit">Log In</button>
             </form>
+            <p>{status}</p>
         </div>
     )
 }
